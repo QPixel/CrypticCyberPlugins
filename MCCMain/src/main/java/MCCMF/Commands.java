ca498f84534f9e7bafc2c603514ab364677f1b83
QPixelDev <realqpixel@gmail.com>
2017-12-01 17:32:45 -0800

    MCC Main Plugin

---
+++ MCCMain/src/main/java/MCCMF/Commands.java
@@ -0,0 +1,25 @@
+package MCCMF;
+
+import net.md_5.bungee.api.ChatColor;
+import net.minecraft.server.v1_12_R1.CommandExecute;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+
+public class Commands extends CommandExecute implements Listener,CommandExecutor {
+
+    public String cmd1 = "giveitem";
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] arg) {
+        if(sender instanceof Player){
+            if(cmd.getName().equalsIgnoreCase(cmd1)){
+
+            }
+        }else{
+            sender.sendMessage(ChatColor.RED + "Only players can use this command!");
+            return true;
+        }
+        return false;
+    }
+}
---
+++ MCCMain/src/main/java/MCCMF/Guis/PermInventory.java
@@ -0,0 +1,22 @@
+package MCCMF.Guis;
+
+
+import MCCMF.Main.Main;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.plugin.Plugin;
+
+public class PermInventory implements Listener{
+
+    private Plugin plugin = Main.getPlugin(Main.class);
+
+        public void newInventory(Player player){
+            Inventory i = plugin.getServer().createInventory(null, 9, ChatColor.RED + "Perm Set");
+            player.openInventory(i);
+        }
+
+
+
+}
